65e9ba7a89d3c638992c29a2f27a7c01bfc6b01e
Vicente <dev.vicentefigueroa@gmail.com>
2022-01-23 23:09:13 -0300

    algo

--- backend/settings.py
+++ backend/settings.py
@@ -52,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
