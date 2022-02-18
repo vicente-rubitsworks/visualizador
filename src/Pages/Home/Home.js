9b10582b7c7cc76d9ab4c1dc22aa71dac24331a1
Vicente <vicente@rubits.works>
2022-02-17 22:07:34 -0300

    cambios

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -20,44 +20,63 @@ export default class Home extends Component {
           {" "}
           {/* Content Wrapper. Contains page content */}
           <div className="content-wrapper">
+            {/* Content Header (Page header) */}
+            <section className="content-header">
+              <div className="container-fluid">
+                <div className="row mb-2">
+                  <div className="col-sm-6">
+                    <h1>404 Error Page</h1>
+                  </div>
+                  <div className="col-sm-6">
+                    <ol className="breadcrumb float-sm-right">
+                      <li className="breadcrumb-item">
+                        <a href="#">Home</a>
+                      </li>
+                      <li className="breadcrumb-item active">404 Error Page</li>
+                    </ol>
+                  </div>
+                </div>
+              </div>
+              {/* /.container-fluid */}
+            </section>
             {/* Main content */}
             <section className="content">
-              <Container fluid>
-                <Row>
-                  <Tarjeta
-                    nombre="Matrículas"
-                    dato="172,145"
-                    icon="fa-chalkboard"
-                  >
-                  
-                  </Tarjeta>
-                  <Tarjeta
-                    nombre="Establecimientos"
-                    dato="1,001"
-                    icon="fa-building"
-                  />
-                  <Tarjeta
-                    nombre="Cupos"
-                    dato="168,583"
-                    icon="fa-graduation-cap"
-                  />
-                  <Tarjeta
-                    nombre="Postulaciones"
-                    dato="79,613"
-                    icon="fa-users"
-                  />
-                </Row>
-                <Row>
-                  <SectionLeft>
-                    <Calendar />
-                  </SectionLeft>
-                  <SectionRight>
-                    <Map />
-                  </SectionRight>
-                </Row>
-                {/* /.row (main row) */}
-              </Container>
-              {/* /.container-fluid */}
+              <div className="error-page">
+                <h2 className="headline text-warning"> 404</h2>
+                <div className="error-content">
+                  <h3>
+                    <i className="fas fa-exclamation-triangle text-warning" />{" "}
+                    Oops! Page not found.
+                  </h3>
+                  <p>
+                    We could not find the page you were looking for. Meanwhile,
+                    you may <a href="../../index.html">return to dashboard</a>{" "}
+                    or try using the search form.
+                  </p>
+                  <form className="search-form">
+                    <div className="input-group">
+                      <input
+                        type="text"
+                        name="search"
+                        className="form-control"
+                        placeholder="Search"
+                      />
+                      <div className="input-group-append">
+                        <button
+                          type="submit"
+                          name="submit"
+                          className="btn btn-warning"
+                        >
+                          <i className="fas fa-search" />
+                        </button>
+                      </div>
+                    </div>
+                    {/* /.input-group */}
+                  </form>
+                </div>
+                {/* /.error-content */}
+              </div>
+              {/* /.error-page */}
             </section>
             {/* /.content */}
           </div>
